02743db948e68375dc358638de623ea2a2cbfee3
Jose Hales-Garcia <jolohaga@me.com>
2010-04-09 10:35:09 -0700

    * Changed migration stage to simple file copy ('directory db').   The migration_template method isn't working at this time.   Renamed the prefix numbers of the migration files to be compatible   with the non-timestamped migration naming convention.

--- lib/generators/core_auth/core_auth_generator.rb
+++ lib/generators/core_auth/core_auth_generator.rb
@@ -8,6 +8,7 @@ class CoreAuthGenerator < Rails::Generators::Base
     directory 'app'
     directory 'public'
     directory 'vendor'
+    directory 'db'
     
     # Inject code at beginning of application_controller.rb.
     #
@@ -90,27 +91,9 @@ class CoreAuthGenerator < Rails::Generators::Base
   # End CoreAuth
 }
     end
-    
-    # Copy migration files.
-    #
-    ActiveRecord::Base.timestamped_migrations = false
-    dirname = 'db/migrate'
-    migration_lookup_at(dirname).each do |migration|
-      migration_template "#{dirname}/#{migration}", "db/migrate"
-    end
   end
   
   def self.source_root
     File.join(File.dirname(__FILE__), 'templates')
-  end
-  
-  # Implement the required interface for Rails::Generators::Migration.
-  # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
-  def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      Time.now.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
-    end
   end
 end
